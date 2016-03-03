73035dfd0de819900aec155cc0fabf3f20e60b5b
Austin Williams <austinwadewilliams@gmail.com>
2016-03-02 19:24:19 -0500

    some progress

--- tests/create-test.js
+++ tests/create-test.js
@@ -1,13 +1,5 @@
-var chai = require('chai');
 var expect = chai.expect;
-var $ = require('jquery');
-var hello = require('../app/scripts/index').hello;
 
-describe('hello', function(){
-  it('should say hello', function(){
-    expect(hello()).to.be.equal('hello world');
-  });
-});
 describe('hello', function(){
   it('should trigger a create:post event on the document with the title and body', function(){
     $(document).on('posts:fetched',function(){
